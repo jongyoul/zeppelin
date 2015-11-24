cccddc71ae11d1cadd1942dfb1a22c58ff4e7c02
Jongyoul Lee <jongyoul@gmail.com>
2015-11-24 14:31:47 +0900

    ZEPPELIN-440 HiveInterpreter with multiple configuration - Fixed getScriptBody return after trimming

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Paragraph.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Paragraph.java
@@ -133,7 +133,7 @@ public class Paragraph extends Job implements Serializable, Cloneable {
     if (magic.length() + 1 >= text.length()) {
       return "";
     }
-    return text.substring(magic.length() + 1);
+    return text.substring(magic.length() + 1).trim();
   }
 
   public NoteInterpreterLoader getNoteReplLoader() {
